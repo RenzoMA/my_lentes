fbc0e492b7499eb3850e9e50377eca614ea85003
Renzo Maldonado <renzoma89@gmail.com>
2021-02-19 17:51:38 -0500

    login

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { MaterialModule } from "../material/material.module";
 import { FooterComponent } from "./footer/footer.component";
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, RouterModule],
   exports: [NavbarComponent, FooterComponent],
 })
 export class SharedModule {}
